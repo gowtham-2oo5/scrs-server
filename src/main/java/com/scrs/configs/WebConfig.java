a2d80da4ba735711367b73c36c7b2699241d8da4
gowtham-2oo5 <gowtham.ala12345@gmail.com>
2024-12-08 05:44:56 +0530

    Server hmm

--- src/main/java/com/scrs/configs/WebConfig.java
+++ src/main/java/com/scrs/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://scrs.vercel.app/")
                 .allowedMethods("*")
                 .allowedHeaders("*");
     }
