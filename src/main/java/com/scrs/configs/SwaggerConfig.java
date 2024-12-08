ac27922f72978284fd271871976ef4c5abefc706
gowtham-2oo5 <gowtham.ala12345@gmail.com>
2024-12-08 06:09:09 +0530

    Server hmm

--- src/main/java/com/scrs/configs/SwaggerConfig.java
+++ src/main/java/com/scrs/configs/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.scrs.configs;
 
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
@@ -31,7 +34,11 @@ public class SwaggerConfig {
                                         .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")))
-                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"));
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+                .servers(List.of(
+                        new Server().url("https://scrs-server-production.up.railway.app").description("Production Server"),
+                        new Server().url("http://scrs.vercel.app").description("Development Server")
+                ));
     }
 
 }
